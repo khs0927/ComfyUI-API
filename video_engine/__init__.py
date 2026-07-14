2d51c302faf723976f217af41be2b8deda990726
khs0927 <130247531+khs0927@users.noreply.github.com>
2026-07-15 02:33:34 +0900

    Install schema-correct fal LTX provider

--- video_engine/__init__.py
+++ video_engine/__init__.py
@@ -1,5 +1,8 @@
 """Long-form AI video orchestration package."""
 
-from .core import LongVideoOrchestrator, VideoRequest, VideoJob
+from .core import LongVideoOrchestrator, VideoJob, VideoRequest
+from .fal_ltx import install as install_fal_ltx_provider
+
+install_fal_ltx_provider()
 
 __all__ = ["LongVideoOrchestrator", "VideoRequest", "VideoJob"]
